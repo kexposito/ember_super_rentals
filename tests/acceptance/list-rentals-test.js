94e3dcb3311c5eaddbe8e1d18fb3ff22583d3148
kevin <kevin@mimiquate.com>
2018-11-28 18:03:41 -0300

    creacion de la ruta rentals, se agrego rentas para mostrar en /rentas y se actualizaron los correspondientes tests.https://guides.emberjs.com/release/tutorial/installing-addons/

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,17 +1,26 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, currentURL,click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
 
-/*test('should show rentals as the home page', async function (assert){
+test('should show rentals as the home page', async function (assert){
+  await visit('/');
+  //await click(".menu-about");
+  assert.equals(currentURL(), '/rentals', 'should redirect automatically');
 });
 test('should link to information about the company.', async function (assert){
+  await visit('/');
+  await click(".menu-about");
+  assert.equals(currentURL(), '/rentals', 'should navigate to about');
 });
 test('should link to contact information.', async function (assert){
+  await visit('/');
+  await click(".menu-contact");
+  assert.equals(currentURL(), '/rentals', 'should navigate to contact');
 });
-test('should list avaliable rentals.', async function (assert) {
+/*test('should list avaliable rentals.', async function (assert) {
 });
 test('should filter the list of rentals by city.', async function (assert){
 });

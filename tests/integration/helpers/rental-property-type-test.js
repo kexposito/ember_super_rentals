821b825d0fdbd047f4006d1e87687c340e49ff8d
kevin <kevin@mimiquate.com>
2018-11-30 10:11:53 -0300

    errores de test a corregir 7-8-9-11-15-43-54-64 , los errores de ajax son por el mapa que me da esos errores

--- tests/integration/helpers/rental-property-type-test.js
+++ tests/integration/helpers/rental-property-type-test.js
@@ -9,7 +9,7 @@ module('Integration | Helper | rentals-property-type', function(hooks) {
   // Replace this with your real tests.
   test('it renders correctly for a Standalone rental', async function(assert) {
     this.set('inputValue', 'Estate');
-    await render(hbs`{{rentals-property-type inputValue}}`);
+    await render(hbs`{{rental-property-type inputValue}}`);
     assert.equal(this.element.textContent.trim(), 'Standalone');
   });
 
